44dadf1083e8fc386181247f4e25e0cd481580ed
aman-singh1601 <aman16singh2001@gmail.com>
2023-07-30 17:26:49 +0530

    added store modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+"use client";
+
 import { Button } from '@/components/ui/button'
-export default function Home() {
+import { UserButton } from '@clerk/nextjs'
+import { Modal } from '@/components/ui/modal'
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
+const SetupPgae=()=> {
+
+  const onOpen =useStoreModal((state)=> state.onOpen)
+  const isOpen =useStoreModal((state)=> state.isOpen)
+
+  useEffect(()=>{
+      if(!isOpen){
+        onOpen();
+      }
+  },[isOpen,onOpen])
   return (
-    <div>
-      <Button>Click Me</Button>
+    <div className='p-4'>
+      Root Page
     </div>
   )
 }
+export default SetupPgae
