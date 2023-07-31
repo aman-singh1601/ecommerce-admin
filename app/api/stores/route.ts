d3ee1402ecabce8c21d42488cdb48581512ea4bf
aman-singh1601 <aman16singh2001@gmail.com>
2023-07-31 15:26:16 +0530

    added react-hot-toast

---
+++ app/api/stores/route.ts
@@ -0,0 +1,30 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export async function POST(req:Request) {
+    try{    
+        const {userId}=auth();
+        const body=await req.json();
+
+        const {name}=body;
+        if(!userId){
+            return new NextResponse('Unauthorised',{status:401});
+        }
+        if(!name){
+            return new NextResponse('Name is required',{status:401});
+        }
+
+        const store=await prismadb.store.create({
+           data:{
+            name,
+            userId
+           } 
+        })
+        return NextResponse.json(store)
+
+    }catch(err){
+        console.log('[Stores_post] ',err)
+        return new NextResponse('internal error',{status:500})
+    }
+}
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 import * as z from 'zod'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver} from '@hookform/resolvers/zod'
+import axios from 'axios'
 
 import { useStoreModal } from "@/hooks/use-store-modal"
 import { Modal } from "@/components/ui/modal"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { toast } from 'react-hot-toast'
 
 
 
@@ -19,6 +22,7 @@ const formSchema=z.object({
 
 export const StoreModal=()=>{
     const StoreModal=useStoreModal();
+    const [loading,setLoading]=useState(false)
 
     const form =useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -28,7 +32,17 @@ export const StoreModal=()=>{
     });
 
     const onSubmit=async (values:z.infer<typeof formSchema>)=>{
-        console.log(values)
+        try{
+            setLoading(true)
+            const response = await axios.post('/api/stores',values);
+            toast.success("Store Created!")
+            
+        }catch(err){
+            toast.error('Someting Went Wrong')
+            // console.log(err)
+        }finally{
+            setLoading(false)
+        }
     }
     return (
     <Modal
@@ -51,17 +65,20 @@ export const StoreModal=()=>{
                         <FormItem>
                             <FormLabel>Name</FormLabel>
                             <FormControl>
-                                <Input placeholder='E-Commerce' {...field}/>
+                                <Input disabled={loading}
+                                 placeholder='E-Commerce' 
+                                 {...field}
+                                 />
                             </FormControl>
                             <FormMessage/>
                         </FormItem>
                     )}
                 />
                 <div className='pt-6 space-x-2 flex items-center justify-end w-full '>
-                    <Button variant='outline' onClick={StoreModal.onClose}>
+                    <Button disabled={loading} variant='outline' onClick={StoreModal.onClose}>
                         Cancel
                     </Button>
-                    <Button type='submit'>
+                    <Button disabled={loading} type='submit'>
                         Continue
                     </Button>
                 </div>
