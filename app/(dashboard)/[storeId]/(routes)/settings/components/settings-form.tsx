9190f064723ebff987dcfa1c23a2aba2d03b2556
aman-singh1601 <aman16singh2001@gmail.com>
2023-08-01 16:59:28 +0530

    creating settings page

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { Heading } from "@/components/ui/Heading";
+import {Store} from "@prisma/client"
+
+
+interface SettingsFormProps{
+    initialData:Store;
+}
+export const SettingsForm:React.FC<SettingsFormProps> =({
+    initialData
+})=>{
+    return (
+        <div className="flex items-center justify-between">
+            <Heading
+                title="Settings"
+                description="Manage store preferences"
+            />
+        </div>
+    )
+}
