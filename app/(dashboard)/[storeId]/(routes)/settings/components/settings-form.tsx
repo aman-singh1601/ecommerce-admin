c26c7a1075daf877d60257ee9e7a0da6f312af8e
aman-singh1601 <aman16singh2001@gmail.com>
2023-08-03 17:37:05 +0530

    added changes

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -17,6 +17,7 @@ import axios from 'axios';
 import { useParams, useRouter } from 'next/navigation';
 import { AlertModal } from '@/components/modals/alert-modal';
 import { ApiAlert } from '@/components/ui/api-alert';
+import { useOrigin } from '@/hooks/use-origin';
 
 
 interface SettingsFormProps{
@@ -33,6 +34,7 @@ export const SettingsForm:React.FC<SettingsFormProps> =({
 })=>{
     const params=useParams();
     const router=useRouter();
+    const origin =useOrigin();
     const [open,setOpen]=useState(false);
     const [loading,setLoading]=useState(false);
 
@@ -116,7 +118,7 @@ export const SettingsForm:React.FC<SettingsFormProps> =({
                 </form>
             </Form>
             <Separator/>
-            <ApiAlert title='test' description='test-description' variant='public'/>
+            <ApiAlert title='NEXT_PUBLIC_API_URL' description={`${origin}/api/${params.storeId}`} variant='public'/>
         </>
     )
 }
